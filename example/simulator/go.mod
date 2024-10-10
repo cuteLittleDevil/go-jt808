faf8cc009e380abcd9188d9a484911962fdf12e7
cuteLittleDevil <792192820@qq.com>
2024-10-10 22:01:00 +0800

    example: 模拟器增加版本等功能

---
+++ example/simulator/go.mod
@@ -0,0 +1,14 @@
+module simulator
+
+go 1.23.2
+
+require (
+	github.com/cuteLittleDevil/go-jt808/service v0.1.3
+	github.com/cuteLittleDevil/go-jt808/shared v1.0.0
+	github.com/cuteLittleDevil/go-jt808/terminal v0.1.1
+)
+
+require (
+	github.com/cuteLittleDevil/go-jt808/protocol v1.0.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+)
--- example/simulator/online/main.go
+++ example/simulator/online/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"encoding/hex"
 	"flag"
 	"fmt"
-	"log"
+	"github.com/cuteLittleDevil/go-jt808/shared/consts"
+	"github.com/cuteLittleDevil/go-jt808/terminal"
 	"net"
 	"sync"
 	"sync/atomic"
@@ -15,6 +15,8 @@ func main() {
 	var (
 		localIP                  string
 		address                  string
+		phoneStart               int
+		version                  int
 		maxPortRange             int
 		heartBeatCycleSecond     int
 		locationCycleSecond      int
@@ -22,7 +24,9 @@ func main() {
 	)
 	flag.IntVar(&maxPortRange, "port", 10000, "本地可用端口数量")
 	flag.StringVar(&localIP, "ip", "192.168.1.10", "本地ip")
-	flag.StringVar(&address, "addr", "192.168.1.135:8080", "服务端ip")
+	flag.IntVar(&phoneStart, "start", 1, "手机号开始的数字")
+	flag.IntVar(&version, "version", 2013, "协议版本 默认2013")
+	flag.StringVar(&address, "addr", "127.0.0.1:8080", "服务端ip")
 	flag.IntVar(&heartBeatCycleSecond, "hc", 20, "心跳周期 默认20秒 小于等于0则不发送")
 	flag.IntVar(&locationCycleSecond, "lc", 5, "定位上传周期 默认5秒 小于等于0则不发送")
 	flag.IntVar(&batchLocationCycleSecond, "blc", 60, "定位批量上传周期 默认60秒 小于等于0则不发送")
@@ -32,19 +36,22 @@ func main() {
 	defer ticker.Stop()
 
 	sum := int32(0)
+	count := int32(1)
 	for range ticker.C {
-		go func() {
+		go func(phoneStart int) {
 			defer func() {
 				atomic.AddInt32(&sum, -1)
 			}()
+			fmt.Println(fmt.Sprintf("本地IP[%s] 可用端口数量[%d] 服务端IP[%s] 活跃连接[%d] 创建手机号[%d]",
+				localIP, maxPortRange, address, sum, phoneStart))
 			localAddr, err := net.ResolveTCPAddr("tcp", localIP+":0") // 端口设置为0以让系统分配
 			if err != nil {
-				log.Fatal(err)
+				return
 			}
 
 			remoteAddr, err := net.ResolveTCPAddr("tcp", address)
 			if err != nil {
-				log.Fatal(err)
+				return
 			}
 
 			conn, err := net.DialTCP("tcp", localAddr, remoteAddr)
@@ -54,12 +61,20 @@ func main() {
 			defer func() {
 				_ = conn.Close()
 			}()
+			protocolVersion := consts.JT808Protocol2013
+			switch version {
+			case 2011:
+				protocolVersion = consts.JT808Protocol2011
+			case 2019:
+				protocolVersion = consts.JT808Protocol2019
+			}
+			t := terminal.New(terminal.WithHeader(protocolVersion, fmt.Sprintf("%d", phoneStart)))
 			var (
-				register, _      = hex.DecodeString("7e0100002d0144199999990001000b0065373034343358485830303030320000000000000000000000006964303030303301d4c1413138383838927e")
-				auth, _          = hex.DecodeString("7e0102000b01441999999900023134343139393939393939f67e")
-				heartBeat, _     = hex.DecodeString("7e000200000144199999990015d27e")
-				location, _      = hex.DecodeString("7e0200007c0123456789017fff000004000000080006eeb6ad02633df701380003006320070719235901040000000b02020016030200210402002c051e3737370000000000000000000000000000000000000000000000000000001105420000004212064d0000004d4d1307000000580058582504000000632a02000a2b040000001430011e3101286b7e")
-				batchLocation, _ = hex.DecodeString("7e0702004e0123456789017fff022007211830000004d5c5c8fd313233343536373839303132330000000000000028d6d0bbaac8cbc3f1b9b2bacdb9fab5c0c2b7d4cbcae4b4d3d2b5c8cbd4b1b4d3d2b5d7cab8f1d6a420190630307e")
+				register      = t.CreateDefaultCommandData(consts.T0100Register)
+				auth          = t.CreateDefaultCommandData(consts.T0102RegisterAuth)
+				heartBeat     = t.CreateDefaultCommandData(consts.T0002HeartBeat)
+				location      = t.CreateDefaultCommandData(consts.T0200LocationReport)
+				batchLocation = t.CreateDefaultCommandData(consts.T0704LocationBatchUpload)
 			)
 			_, _ = conn.Write(register)
 			time.Sleep(time.Second)
@@ -112,16 +127,14 @@ func main() {
 					}
 				}
 			}
-		}()
-
+		}(phoneStart)
+		phoneStart++
 		atomic.AddInt32(&sum, 1)
-		fmt.Println(fmt.Sprintf("本地IP[%s] 可用端口数量[%d] 服务端IP[%s] 活跃连接[%d]",
-			localIP, sum, address, sum))
-		if sum >= int32(maxPortRange) {
+		count++
+		if count > int32(maxPortRange) {
 			cTicker := time.NewTicker(1 * time.Second)
 			for range cTicker.C {
-				fmt.Println(fmt.Sprintf("本地IP[%s] 可用端口数量[%d] 服务端IP[%s] 活跃连接[%d]",
-					localIP, sum, address, sum))
+				fmt.Println(fmt.Sprintf("本地IP[%s] 可用端口数量[%d] 活跃连接[%d]", localIP, maxPortRange, sum))
 			}
 		}
 	}
