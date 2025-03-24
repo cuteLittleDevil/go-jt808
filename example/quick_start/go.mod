2628a4f2b8611538f51e6acac81c7b862f7ebe30
CDCDDCDC <792192820@qq.com>
2025-03-25 00:22:24 +0800

    example: 调整快速开始例子

--- example/quick_start/go.mod
+++ example/quick_start/go.mod
@@ -3,10 +3,10 @@ module quick/start
 go 1.23.2
 
 require (
-	github.com/cuteLittleDevil/go-jt808/attachment v0.2.0
-	github.com/cuteLittleDevil/go-jt808/protocol v1.13.0
+	github.com/cuteLittleDevil/go-jt808/protocol v1.14.0
 	github.com/cuteLittleDevil/go-jt808/service v1.8.0
 	github.com/cuteLittleDevil/go-jt808/shared v1.6.0
+	github.com/cuteLittleDevil/go-jt808/terminal v1.2.0
 )
 
 require golang.org/x/text v0.21.0 // indirect
--- example/quick_start/main.go
+++ example/quick_start/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
-	"github.com/cuteLittleDevil/go-jt808/attachment"
+	"fmt"
+	"github.com/cuteLittleDevil/go-jt808/protocol/jt808"
+	"github.com/cuteLittleDevil/go-jt808/protocol/model"
 	"github.com/cuteLittleDevil/go-jt808/service"
 	"github.com/cuteLittleDevil/go-jt808/shared/consts"
+	"github.com/cuteLittleDevil/go-jt808/terminal"
 	"log/slog"
+	"net"
 	"os"
+	"strings"
+	"time"
 )
-
-var goJt808 *service.GoJT808
 
 func init() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
@@ -17,30 +21,62 @@ func init() {
 		ReplaceAttr: nil,
 	}))
 	slog.SetDefault(logger)
-
-	attach := attachment.New(
-		attachment.WithNetwork("tcp"),
-		attachment.WithHostPorts("0.0.0.0:10001"),
-		attachment.WithActiveSafetyType(consts.ActiveSafetyJS), // 默认苏标 支持黑标 广东标 湖南标 四川标
-		attachment.WithFileEventerFunc(func() attachment.FileEventer {
-			return &meFileEvent{} // 自定义文件处理 开始 结束 当前进度 补传 完成等事件
-		}),
-	)
-	go attach.Run()
 }
 
 func main() {
-	goJt808 = service.New(
+	goJt808 := service.New(
 		service.WithHostPorts("0.0.0.0:808"),
 		service.WithNetwork("tcp"),
-		service.WithCustomTerminalEventer(func() service.TerminalEventer {
-			return &meTerminal{} // 自定义终端事件 终端进入 离开 读写报文事件
-		}),
 		service.WithCustomHandleFunc(func() map[consts.JT808CommandType]service.Handler {
 			return map[consts.JT808CommandType]service.Handler{
 				consts.T0200LocationReport: &meLocation{}, // 自定义0x0200位置解析等
 			}
 		}),
 	)
+	go client("1001", "127.0.0.1:808") // 模拟一个设备连接
 	goJt808.Run()
+}
+
+type meLocation struct {
+	model.T0x0200
+}
+
+func (l *meLocation) Parse(jtMsg *jt808.JTMessage) error {
+	return l.T0x0200.Parse(jtMsg)
+}
+
+func (l *meLocation) OnReadExecutionEvent(msg *service.Message) {
+	_ = l.Parse(msg.JTMessage)
+	fmt.Println(l.String()) // 打印经纬度等信息
+}
+
+func (l *meLocation) OnWriteExecutionEvent(_ service.Message) {}
+
+func (l *meLocation) String() string {
+	body := l.T0x0200.Encode()
+	return strings.Join([]string{
+		"数据体对象:{",
+		fmt.Sprintf("\t%s:[%x]", l.Protocol(), body),
+		l.T0x0200LocationItem.String(),
+		l.AlarmSignDetails.String(),
+		l.StatusSignDetails.String(),
+		"}",
+	}, "\n")
+}
+
+func client(phone string, address string) {
+	time.Sleep(time.Second)
+	t := terminal.New(terminal.WithHeader(consts.JT808Protocol2013, phone))
+	location := t.CreateDefaultCommandData(consts.T0200LocationReport)
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return
+	}
+	defer func() {
+		_ = conn.Close()
+	}()
+	ticker := time.NewTicker(1 * time.Second)
+	for range ticker.C {
+		_, _ = conn.Write(location)
+	}
 }
